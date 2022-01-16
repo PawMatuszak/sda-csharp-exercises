a0ba8b07b73ec4eed132ad0cf146bcbd36ca1d6b
WarGho <pawel.matuszak5@wp.pl>
2022-01-16 15:27:57 +0100

    Wykonane zadanie

--- Program.cs
+++ Program.cs
@@ -4,9 +4,40 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void makeTriangle(int lenght, string sign)
+        {
+            for(int i = 0 ; i<lenght ; i++)
+            {
+                for (int j = 0 ; j<=i ; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = lenght - 1 ; i>0 ; i--)
+            {
+                for (int j = i; j>0; j--)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Podaj najwyższy punkt piramidy");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj swój znak");
+            string sign = Console.ReadLine();
+
+            Console.WriteLine();
+
+            makeTriangle(lenght, sign);
+
         }
     }
 }
