8805f4d6c4d501fa90d2fae6c31150475d0648fe
WarGho <pawel.matuszak5@wp.pl>
2022-01-16 14:17:51 +0100

    Zadanie wykonane

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Jakiej liczby chcesz poznać potęgę Poszukiwaczu wiedzy");
+            int master = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"i do której potęgi chcesz podnieść {master} ?");
+            int power = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+
+            if (power < 0)
+            {
+                Console.WriteLine("Tak się chyba nie da co?");
+            }
+            else
+            {
+                for (int i = 1; i <= power; i++)
+                {
+                    sum = master * master;
+
+                }
+                Console.WriteLine($"Twoja odpowiedz to {sum}.");
+            }
+            
         }
     }
 }
