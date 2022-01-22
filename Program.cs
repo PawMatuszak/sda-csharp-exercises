79a9be4f05b19cbb24a219270c92e3089471d1dc
WarGho <pawel.matuszak5@wp.pl>
2022-01-22 15:39:33 +0100

    Zadanie gotowe

--- Program.cs
+++ Program.cs
@@ -4,9 +4,53 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        static public bool GetControlDigit(string pesel)
+        {
+            int[] scale = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+            int control;
+
+            for (int i = 0; i < pesel.Length - 1; i++)
+            {
+                sum += Convert.ToInt32(Char.GetNumericValue(pesel[i]) * scale[i]);
+            }
+
+            int modulo = sum % 10;
+
+            control = 10 - modulo;
+            
+            if (control == 10)
+            {
+                control = 0;
+            }
+
+            if (control == Char.GetNumericValue(pesel[10]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
+            string pesel1 = "55030101230"; //poprawny
+            string pesel2 = "55030101192"; //niepoprawny
+            
+
            
+            if(GetControlDigit(pesel2) == true)
+            {
+                Console.WriteLine("Pesel poprawny");
+            }
+            else
+            {
+                Console.WriteLine("Pesel niepoprawny");
+            }
+            
+            
         }
     }
 }
