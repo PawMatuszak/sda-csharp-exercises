565147477cfcb079f94f95627b1733b88bbe2526
WarGho <pawel.matuszak5@wp.pl>
2022-01-22 14:27:04 +0100

    Zadanie gotowe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace sda_csharp_exercises
 {
@@ -6,7 +7,48 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-           
+
+            Console.WriteLine("Ile liczb chcesz wprowadzić?");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i <= n-1; i++)
+            {
+                Console.WriteLine($"Podaj " +(i+1) + " wartość tablicy");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            PrintNumbers(numbers);
         }
+
+        public static void PrintNumbers(int[] numbers)
+        {
+           
+            int[] newNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < newNumbers.Length; i++)
+            {
+                newNumbers[i] = -1;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!newNumbers.Contains(numbers[i]))
+                {
+                    newNumbers[i] = numbers[i];
+                }
+            }
+
+            
+            foreach (var number in newNumbers)
+            {
+                if (number >= 0)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+
     }
 }
