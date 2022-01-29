492f0369d3cd40a439eabbda89ce1c9f6b4324b2
WarGho <pawel.matuszak5@wp.pl>
2022-01-29 11:46:33 +0100

    Gotowe zadanie

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sda_csharp_exercises
+{
+    class Person
+    {
+        public string _firstName;
+        public string _lastName;
+        public int _yearOfBirth;
+    }
+}
