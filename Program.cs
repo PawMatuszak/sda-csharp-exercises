a44243655e3bb49528dfdfa15c5a77acbe3e91e9
WarGho <pawel.matuszak5@wp.pl>
2022-01-30 11:23:57 +0100

    Done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-           
+            Rectangle prostokąt1 = new Rectangle(2, 3);
+
+            prostokąt1.Show();
+            Rectangle.PrintClassName();
+
+            Rectangle[] rectangles = new Rectangle[3];
+            rectangles[0] = prostokąt1;
+            rectangles[1] = new Rectangle(4, 5);
+            rectangles[2] = new Rectangle(5, 2);
+
+            foreach (Rectangle rectangle in rectangles)
+            {
+                rectangle.Show();
+            }
+
         }
     }
 }
