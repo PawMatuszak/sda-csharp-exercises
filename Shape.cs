591041506de9e82b63e7d4498b8154abd3056575
WarGho <pawel.matuszak5@wp.pl>
2022-01-30 09:51:14 +0100

    Gotowe zadanie

---
+++ Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sda_csharp_exercises
+{
+    abstract class Shape
+    {
+        public static double GetTotalArea(Shape[] shapes)
+        {
+            double totalArea = 0;
+
+            foreach (var shape in shapes)
+            {
+                totalArea += shape.GetArea();
+            }
+
+            return totalArea;
+        }
+
+
+        abstract public double GetArea();
+        
+
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sda_csharp_exercises
+{
+    class Square : Shape
+    {
+        private double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public override double GetArea()
+        {
+            return side * side;
+        }
+    }
+}
